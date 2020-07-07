34521de213c0839982f73edd88d643bc5a363806
Derek Moore <derek.moore@gmail.com>
2020-07-07 00:34:05 -0700

    Initial Commit

---
+++ AdultFezFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdultFezFinder
+//
+//  Created by Derek Moore on 7/7/20.
+//  Copyright © 2020 Derek Moore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
